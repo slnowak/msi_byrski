08aa42b813f5ddb327c321e58157a29d3af24b89
novy154 <slnowak154@gmail.com>
2014-11-18 20:29:35 +0100

    Added standard deviation bar, deleted unnecessary code

--- stock_market_prediction/app.py
+++ stock_market_prediction/app.py
@@ -26,27 +26,18 @@ def train_network(training_dataset, test_dataset, epochs=10, learning_rate=0.01,
     mean = predictions.mean(axis=0).tolist()
     minimum = predictions.min(axis=0).tolist()
     maximum = predictions.max(axis=0).tolist()
+    std = predictions.std(axis=0).tolist()
     real = reduce(lambda x, y: x+y, test_dataset['target'].tolist())
 
     x = [i for i in range(1, len(real) + 1)]
-    plt.title(("Liczba epok: %d, szybkosc uczenia: %f, momentum: %f" % (epochs, learning_rate, momentum)))
+    plt.title(("Liczba epok: %d, Szybkosc uczenia: %f, Momentum: %f" % (epochs, learning_rate, momentum)))
     plt.plot(x, real, label="Wartosci rzeczywiste", color='red')
     plt.plot(x, mean, label="Wartosci srednie", color='green')
     plt.plot(x, minimum, label="Wartosci min", color='grey')
     plt.plot(x, maximum, label="Wartosci max", color='grey')
+    plt.errorbar(x, mean, yerr=std, fmt="r-", color="green", elinewidth=0.5, capthick=1.0)
     plt.legend()
     plt.show()
-
-
-def create_output_string(data):
-    return '\n'.join([' '.join([str(number) for number in row]) for row in data])
-
-
-def write_to_file(filepath, data):
-    with open(filepath, 'wb') as target:
-        target.write(
-            create_output_string(data)
-        )
 
 if __name__ == '__main__':
     raw_data = DataReader().read_data('input')
